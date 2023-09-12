a79c02911f91307f2ad05335f07e9a622819b8c1
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2023-09-12 17:29:40 +0200

    dont be verbose when explodig

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,4 @@
 pip install -r requirements.txt
 rm -rf parameters.ini parameters_idm.ini slhaFiles files_tutorial.tar.gz
 wget https://raw.githubusercontent.com/SModelS/tutorials/main/files_tutorial.tar.gz
-tar xzvf files_tutorial.tar.gz
+tar xzf files_tutorial.tar.gz
